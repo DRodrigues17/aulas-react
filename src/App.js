da1405db6f467b98866be956a301f5620bc82c6a
Daniel Rodrigues <91800053+DRodrigues17@users.noreply.github.com>
2022-06-24 20:28:39 -0300

    exercício 1.7.2

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
-import { filmes } from "./filmes.js";
-
-//function Filmes() {
-//  function filtrarFilmeAnos(ano = 2000) {
-//    return filmes.filter((filme) => filme.lancamento > ano);
-//  }
-
-//  function mapearFilmes(filmes) {
-//    return filmes
-//      .map((filme) => filme.generos).flat()
-//      .map((genero) => <p>{genero}</p>);
-//  }
-//  return (
-//    <div>
-//      {mapearFilmes(filtrarFilmeAnos())}
-//    </div>
-//  );
-//}
-
-function FilmeCard() {
-  function filtrarFilmeOscar() {
-    return filmes.filter((filme) => filme.oscar);
-  }
-  function mapearFilmes(filmes) {
-    return filmes
-      .map((filme) => {
-        if (filme.oscar === true) {
-          return filme.nome + "🏆";
-        }
-        return filme.nome;
-      })
-      .map((filme) => <p>{filme}</p>);
-  }
-  return (
-    <div>
-      <h1>te banhas? 🤨</h1>
-      {mapearFilmes(filtrarFilmeOscar())}
-    </div>
-  );
-}
 export default function App() {
   return (
     <div>
-      <FilmeCard />
+      <h1>hello guys</h1>
     </div>
   );
 }
