498850099d835969129a6e44d8917e3029a91faf
Daniel Rodrigues <91800053+DRodrigues17@users.noreply.github.com>
2022-08-05 20:50:06 -0300

    youtube

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Layout from "./exercicios/exercicio3.13.3.jsx";
 export default function App() {
   return (
     <div>
-      <div class="caixa"></div>
-      <div class="caixa"></div>
-      <div class="caixa"></div>
+      <Layout />
     </div>
   );
 }
--- src/exercicios/exercicio3.13.3.jsx
+++ src/exercicios/exercicio3.13.3.jsx
@@ -1,7 +1,140 @@
+import "./exercicio3.13.3.css";
+
 export default function App() {
   return (
-    <div>
-      <h1> hello guys </h1>
+    <div class="content">
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
+      <div class="video">
+        <div class="thumbnail">Thumbnail</div>
+        <div class="informacao">
+          <div class="circulo"></div>
+          <div class="inforcaoes-video">
+            <p>titulo video</p>
+            <p>nome do canal</p>
+            <p>numero de vizualizações</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
